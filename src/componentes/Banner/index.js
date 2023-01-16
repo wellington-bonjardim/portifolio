ee2cf06f69082bb299e9956ee4911055e0adb613
wellington-bonjardim <wellingtonbonjardim7@gmail.com>
2023-01-16 16:39:43 -0300

    Banner: Criando o banner

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,32 @@
+import styles from './Banner.module.css'
+import circuloColorido from 'assets/circulo_colorido.png'
+import minhaFoto from 'assets/minha_foto.png'
+
+export default function Banner() {
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, pessoa! 
+                </h1>
+
+                <p className={styles.paragrafo}>     
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque et earum in eaque reprehenderit minus, facilis excepturi esse voluptates debitis cupiditate tempora. Error sint amet laudantium velit sunt dolore porro.
+                </p>
+            </div>
+
+            <div className={styles.imagens}>
+                <img 
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
+                <img 
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt="Foto de Wellington Bonjardim sorrindo"
+                />
+            </div>
+        </div>
+    )
+}
--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,3 +1,10 @@
+import Banner from "componentes/Banner";
+
 export default function Inicio() {
-    return <h1>Página Inicial</h1>
+    return (
+        <main>
+            <Banner />
+            <h1>Página INICIAL</h1>
+        </main>
+    )
 }
