c13c5e582c382e410e34913ea4e0d31bb3dde21f
wellington-bonjardim <wellingtonbonjardim7@gmail.com>
2023-01-18 16:32:33 -0300

    NaoEncontrada: Desenvolvendo a página não encontrada (erro404)

---
+++ src/paginas/NaoEncontrada/index.js
@@ -0,0 +1,34 @@
+import styles from './NaoEncontrada.module.css'
+import erro404 from 'assets/erro_404.png'
+
+export default function NaoEncontrada() {
+    return (
+        <>
+            <div className={styles.conteudoContainer}>
+                <span className={styles.texto404}>404</span>
+
+                <h1 className={styles.titulo}>Ops! Página não encontrada.</h1>
+
+                <p className={styles.paragrafo}>
+                    Tem certeza que era isso que você estava procurando?
+                </p>
+
+                <p className={styles.paragrafo}>
+                    Aguarde uns instantes e recarregue a página, ou volte para a página inicial
+                </p>
+
+                <div className={styles.botaoContainer}>
+                    <button>Voltar</button>
+                </div>
+
+                <img 
+                    className={styles.imagemCachorro}
+                    src={erro404}
+                    alt="Cachorro sorrindo"
+                />
+            </div>
+
+            <div className={styles.espacoEmBranco} />
+        </>
+    )
+}
